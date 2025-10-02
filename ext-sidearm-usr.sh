befae2441c0617b8c6988747f45de2141384ca05
username29800 <piapia78490@gmail.com>
2025-10-02 16:51:48 +0900

    nonroot wip 1

---
+++ ext-sidearm-usr.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#prerequisites:
+#usage: [script path] [hostkey(private)]
+prefix=$(cat prefix)
+mkdir $prefix
+cd $prefix/
+mkdir $prefix/sidearmconnect
+chmod -R 755 $prefix/sidearmconnect
+rm $prefix/sidearmconnect/sacsshhkey/key
+rm $prefix/sidearmconnect/sacsshpkey/key
+# this extension will be installed on user directory.
+mkdir $prefix/sidearmconnect/sacsshhkey $prefix/sidearmconnect/sacsshpkey $prefix/sidearmconnect/sacfwdkey $prefix/sidearmconnect/sacsshckey $prefix/sidearmconnect/rcvdest $prefix/.ssh
+ln -sf "$(pwd | sed s,/$,,)"/"$1" $prefix/sidearmconnect/sacsshhkey/key
+ln -sf "$(pwd | sed s,/$,,)"/"$1".pub $prefix/sidearmconnect/sacsshpkey/key
+cp 
