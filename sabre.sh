41ca8cfe716d349c17d4e0b62eaf556bd7a71c10
username29800 <134399459+username29800@users.noreply.github.com>
2025-10-04 19:43:26 +0900

    Update sabre.sh

--- sabre.sh
+++ sabre.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 #sabre: Sidearmconnect-Box Restricted Environment
+#Sabre is a non-root, improved implementation of sidearmconnect.
+#howto: (1) create prefix specification file(echo . > prefix) (2) create rprefix(remote prefix) to connect to remote, or create sprefix(sys-prefix) to automate ssh key registration
+#prefix is the installation directory(where sabre data directory-'sidearmconnect'-will be generated), sprefix is where a user's .ssh is located, rprefix is the prefix of the remote instance of sabre.
+#NOTE: every prefix should not contain trailing slash(es); They may cause errors. To filter out the slashes automatically, pipe stdout into "sed 's,/$,,'"
+#example: echo . > prefix ; echo /home/user-example > sprefix ; echo /home/user-remote/endure > rprefix
 #usage: [script path] [hostkey(private)]
 prefix=$(cat prefix)
 mkdir $prefix
