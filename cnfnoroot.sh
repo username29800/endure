2bf973a4cd23f01abccf0feb105bb639a1f5bb59
username29800 <piapia78490@gmail.com>
2025-10-09 00:42:12 +0900

    add user-specific and nonroot version of sysind.sh

---
+++ cnfnoroot.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#user experience configuration utility - nonroot version
+user="$(whoami)"
+mkdir /home/$user/bin
+mkdir /home/$user/build
+cd /home/$user/build
+#echo 'adduse.r/psw.d/
+#adduse.r/psw.d/' | passwd $user
+#echo vncpasswd - enter a new vnc password
+echo "adusr.wd
+adusr.wd
+n" | vncpasswd
+cd /home/$user
+mkdir Downloads Documents Pictures Music Videos Projects
+mkdir -p .config/tigervnc
+echo 'export GTK_IM_MODULE=ibus' > .zshrc.1
+echo 'export QT_IM_MODULE=ibus' >> .zshrc.1
+echo 'export XMODIFIERS=@im=ibus' >> .zshrc.1
+echo 'export PULSE_SERVER=localhost' >> .zshrc.1
+cp .zshrc.1 .config/tigervnc/xstartup
+echo 'xrandr --output VNC-0 --mode 1920x1080' >> .config/tigervnc/xstartup
+echo 'openbox' >> .config/tigervnc/xstartup
+ln -sf .config/tigervnc/xstartup
+echo '#!/bin/sh' > /home/$user/bin/safefox
+echo 'export MOZ_FAKE_NO_SANDBOX=1' >> /home/$user/bin/safefox
+echo 'firefox $1' >> /home/$user/bin/safefox
+chmod 755 /home/$user/bin/safefox
+echo '#!/bin/sh' > /home/$user/bin/showw
+echo 'rofi -show window &' >> /home/$user/bin/showw
+chmod 755 /home/$user/bin/showw
+echo '#!/bin/sh' > /home/$user/bin/sdclock
+echo 'xclock -digital -update 10 &' >> /home/$user/bin/sdclock
+chmod 755 /home/$user/bin/sdclock
+cp /utils/tlch /home/$user/bin/tlch
+cp /utils/tlch /home/$user/bin/twrapper
+cp /utils/tlch /home/$user/bin/tlaunch
+chmod 744 .config/tigervnc/xstartup
+mkdir .vnc
+cp xstartup .vnc/xstartup
+#mkdir /usr/share/xsessions
+#echo '[Desktop Entry]' > /usr/share/xsessions/custom.desktop
+#echo 'Name=Custom' >> /usr/share/xsessions/custom.desktop
+#echo 'Exec=/home/'"$user"'/xstartup' >> /usr/share/xsessions/custom.desktop
+#echo 'Type=application' >> /usr/share/xsessions/custom.desktop
+cp /utils/swsh /home/$user/swsh
+#echo "LD_PRELOAD=/system/lib64/libskcodec.so pulseaudio --load='module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1'" > /sndsrv
+#sed -i 1i"#\!/bin/sh" /sndsrv
+#chmod 755 /sndsrv
+cd /home/$user/build
+git clone https://github.com/naver/d2codingfont
+git clone https://github.com/ohmyzsh/ohmyzsh
+mkdir /home/$user/.fonts
+unzip d2codingfont/D2Coding-Ver1.3.2*.zip
+cp D2Coding/*.ttf /home/$user/.fonts
+fc-cache
+echo 'set nocp number autoindent tabstop=2 shiftwidth=2 expandtab printheader=""' > /home/$user/.vimrc
+echo 'default password: adduse.r/psw.d/' > /home/$user/passinfo
+echo 'default vnc password: adusr.wd' >> /home/$user/passinfo
+echo 'CHANGE default password for security' >> /home/$user/passinfo
+chown -R $user /home/$user
+chmod -R 755 /home/$user/build/ohmyzsh
+sed -i 's,^.*zsh -l$,#&,' /home/$user/build/ohmyzsh/tools/install.sh
+sh /home/$user/build/ohmyzsh/tools/install.sh
+cat /home/$user/.zshrc.1 >> /home/$user/.zshrc
+sed -ir "$(cat /home/$user/.zshrc | grep -n ^ZSH_THEME | head -n1 | cut -d: -f-1)"'s,=.*\?$,="agnoster",' /home/$user/.zshrc
+chown -R $user /home/$user
+exit
---
+++ usrcnf.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#prerequisites: unzip, coreutils or busybox, firefox, openbox, rofi, qterminal, xterm, tigervnc, pulseaudio, sudo, vim, git, zsh, user 'user' with home directory
+user="$1"
+mkdir /build
+mkdir /rmem /mem
+chmod 0666 /rmem /mem
+cd /build
+chmod 1777 /dev/shm
+mkdir /home
+mkdir /home/$user
+echo 'adduse.r/psw.d/
+adduse.r/psw.d/' | passwd $user
+#echo vncpasswd - enter a new vnc password
+echo "adusr.wd
+adusr.wd
+n" | su -c vncpasswd $user
+sed -i "$(cat /etc/sudoers | grep -n 'root\s\{1,\}ALL' | head -n1 | cut -d: -f-1)"a"$user ALL=(ALL:ALL) ALL" /etc/sudoers
+cd /home/$user
+mkdir Downloads Documents Pictures Music Videos Projects
+mkdir -p .config/tigervnc
+echo 'export GTK_IM_MODULE=ibus' > .zshrc.1
+echo 'export QT_IM_MODULE=ibus' >> .zshrc.1
+echo 'export XMODIFIERS=@im=ibus' >> .zshrc.1
+echo 'export PULSE_SERVER=localhost' >> .zshrc.1
+cp .zshrc.1 .config/tigervnc/xstartup
+echo 'xrandr --output VNC-0 --mode 1920x1080' >> .config/tigervnc/xstartup
+echo 'openbox' >> .config/tigervnc/xstartup
+ln -sf .config/tigervnc/xstartup
+echo '#!/bin/sh' > /bin/safefox
+echo 'export MOZ_FAKE_NO_SANDBOX=1' >> /bin/safefox
+echo 'firefox $1' >> /bin/safefox
+chmod 755 /bin/safefox
+echo '#!/bin/sh' > /bin/showw
+echo 'rofi -show window &' >> /bin/showw
+chmod 755 /bin/showw
+echo '#!/bin/sh' > /bin/sdclock
+echo 'xclock -digital -update 10 &' >> /bin/sdclock
+chmod 755 /bin/sdclock
+cp /utils/tlch /bin/tlch
+cp /utils/tlch /bin/twrapper
+cp /utils/tlch /bin/tlaunch
+chmod 744 .config/tigervnc/xstartup
+mkdir .vnc
+cp xstartup .vnc/xstartup
+mkdir /usr/share/xsessions
+echo '[Desktop Entry]' > /usr/share/xsessions/custom.desktop
+echo 'Name=Custom' >> /usr/share/xsessions/custom.desktop
+echo 'Exec=/home/'"$user"'/xstartup' >> /usr/share/xsessions/custom.desktop
+echo 'Type=application' >> /usr/share/xsessions/custom.desktop
+sed -i "$(cat /etc/passwd | grep -n ^$user: | cut -d: -f-1)"s,'/sh$,/zsh,' /etc/passwd
+cp /utils/swsh /home/$user/swsh
+echo "LD_PRELOAD=/system/lib64/libskcodec.so pulseaudio --load='module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1'" > /sndsrv
+sed -i 1i"#\!/bin/sh" /sndsrv
+chmod 755 /sndsrv
+cd /build
+git clone https://github.com/naver/d2codingfont
+git clone https://github.com/ohmyzsh/ohmyzsh
+mkdir /home/$user/.fonts
+unzip d2codingfont/D2Coding-Ver1.3.2*.zip
+cp D2Coding/*.ttf /home/$user/.fonts
+fc-cache
+echo 'set nocp number autoindent tabstop=2 shiftwidth=2 expandtab printheader=""' > /home/$user/.vimrc
+echo 'default password: adduse.r/psw.d/' > /home/$user/passinfo
+echo 'default vnc password: adusr.wd' >> /home/$user/passinfo
+echo 'CHANGE default password for security' >> /home/$user/passinfo
+chown -R $user /home/$user
+chmod -R 755 /build/ohmyzsh
+sed -i 's,^.*zsh -l$,#&,' /build/ohmyzsh/tools/install.sh
+su -c "sh /build/ohmyzsh/tools/install.sh" $user
+cat /home/$user/.zshrc.1 >> /home/$user/.zshrc
+sed -ir "$(cat /home/$user/.zshrc | grep -n ^ZSH_THEME | head -n1 | cut -d: -f-1)"'s,=.*\?$,="agnoster",' /home/$user/.zshrc
+chown -R $user /home/$user
+exit
