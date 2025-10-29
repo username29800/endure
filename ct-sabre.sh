cb4131bd5adf38009028ce8cb83ac97bac6ecd23
username29800 <134399459+username29800@users.noreply.github.com>
2025-10-30 00:52:47 +0900

    Fix file names in copy command

--- ct-sabre.sh
+++ ct-sabre.sh
@@ -3,6 +3,6 @@
 prefix=$(cat prefix)
 rfspath=$1
 mkdir $prefix/system
-cp -r ext-alpine* inst-alpine* pkg-alpine* cinit pinit initpty cinitc pinitc initptyc sysind.sh usrcnf.sh cnfnoroot.sh sabre.sh utils tools $prefix/system/
+cp -r ext-alpine* inst-alpine* pkg-alpine* cinit pinit initpty cinitc pinitc initptyc sysind.sh usrconf.sh cfgnoroot.sh cfgmini.sh sabre.sh utils tools $prefix/system/
 cd $prefix/system
 #tar -xf $rfspath
