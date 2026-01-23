881eee986e0441823bc4eb1485bbde45ae503c1b
username29800 <piapia78490@gmail.com>
2026-01-24 00:40:15 +0900

    add container tools and system config scripts

--- sve/src/ct-sabre.sh
+++ sve/src/ct-sabre.sh
@@ -4,7 +4,7 @@
 #rfspath=$1
 mkdir -p ${ct_system}/system
 #cp -r ext-alpine* inst-alpine* pkg-alpine* init sysind.sh usrconf.sh cfgnoroot.sh cfgmini.sh utils cs-sabre tools $prefix/system/
-cp -a ${ct_path}/initptyc ${ct_system}/initptyc
+cp -a ${ct_path}/initptyc ${ct_path}/eau-alpine/* ${eux_sve_bin}/cfgfull ${ct_system}/initptyc
 cd ${ct_system}/system
 chmod 755 initptyc
 mkdir mem rmem
